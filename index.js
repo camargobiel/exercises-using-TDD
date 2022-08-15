6cda7034ed7ab0d047c7257ea1dd79bb0b827f1b
camargobiel <camargoobiel@gmail.com>
2022-08-15 20:01:05 -0300

    feat: first exercise done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const teste = () => {
-  return 4;
+/*
+Create a function that takes an array of numbers
+and return "Boom!" if the digit 7 appears in the array.
+Otherwise, return "there is no 7 in the array".
+*/
+
+const firstExercise = (array = []) => {
+  let haveSeven = 'nothing';
+
+  array.forEach(element => {
+    element === 7 ? haveSeven = 'boom' : haveSeven = 'there is no 7 in the array';
+  });
+
+  return haveSeven;
 }
 
-module.exports = teste
+module.exports = { firstExercise }
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,15 @@
-const teste = require('./index');
+const { firstExercise } = require('./index');
 
-describe('test functions', () => {
-  it('function teste() should exists', () => {
-    expect(teste()).toBeTruthy();
-  })
+describe('first exercise', () => {
+  it('function firstExercise() should exists', () => {
+    expect(firstExercise()).toBeTruthy();
+  });
+
+  it('function firstExercise() should return boom if has 7 in the array', () => {
+    expect(firstExercise([1, 2, 7])).toStrictEqual('boom');
+  });
+
+  it('function firstExercise() should return a message if not has 7 in the array', () => {
+    expect(firstExercise([1, 2, 3])).toStrictEqual('there is no 7 in the array');
+  });
 });
